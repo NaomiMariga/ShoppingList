59e33ac00fb266934115f0fc63d80758c12fae4b
Naomi Mariga <kaburamariga@gmail.com>
2017-10-16 20:58:31 +0300

    add class user

--- user.py
+++ user.py
@@ -1,4 +1,25 @@
-class user:
+import random
 
+
+class User:
     def registration(self, email, username, password):
+        return
+
+    def login(self, email, password):
+        return
+
+    def token(self, n, characters):
+        out = ''.join([random.choice(characters) for i in range(n)])
+        return out
+
+    def forgot_password(self, email):
+        return
+
+    def change_password(self, token, user_id, old_password, new_password):
+        return
+
+    def logged_in(self, token, user_id):
+        return
+
+    def log_out(self, token, user_id):
         return
