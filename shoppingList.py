f8e67317d6b00f046776a3d98c5b783ff14467c5
Naomi Mariga <kaburamariga@gmail.com>
2017-10-16 20:57:15 +0300

    add class shopping list

---
+++ shoppingList.py
@@ -0,0 +1,26 @@
+class ShoppingList:
+    def add_list(self, list_name, user_id, token):
+        return
+
+    def update_list(self, list_id, token, user_id, list_name):
+        return
+
+    def delete_list(self, list_id, user_id, token):
+        return
+
+    def add_item(self, item_name, quantity, units, cost, list_id, token, user_id):
+        return
+
+    def delete_item(self, item_id, user_id, token, list_id):
+        return
+
+    def edit_item(self, item_id, list_id, user_id, token, item_name, quantity, cost, units):
+        return
+
+    def read_lists(self, user_id, token):
+        return
+
+    def read_items(self, user_id, token, list_id):
+        return
+
+
