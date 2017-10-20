976cd3ac8cda8a266c496a1f29c64325aa846fb8
Naomi Mariga <kaburamariga@gmail.com>
2017-10-20 14:29:13 +0300

    edit tests coverage

--- tests.py
+++ tests.py
@@ -10,6 +10,11 @@ class Tests(unittest.TestCase):
     registered_mail = user.token(10, "abcdefghijklmnopqrstuvwxyz") + "@example.com"
     registered_password = "correct1234"
 
+    token = None
+    user_id = None
+    lst = None
+    items_list = None
+
     def test_01_all_functions_returns_dictionary(self):
         message = " is supposed to return a dictionary"
         self.assertIsInstance(self.user.registration(None, None, None), dict, "registration" + message)
@@ -23,7 +28,7 @@ class Tests(unittest.TestCase):
         self.assertIsInstance(self.shoppingList.delete_list(None, None, None), dict, "delete_lists" + message)
         self.assertIsInstance(self.shoppingList.add_item(None, None, None, None, None, None, None), dict, " add_item" + message)
         self.assertIsInstance(self.shoppingList.read_items(None, None, None), dict, "read_items" + message)
-        self.assertIsInstance(self.shoppingList.edit_item(None, None, None, None, None, None, None, None), dict, "edit_item" + message)
+        self.assertIsInstance(self.shoppingList.edit_item(None, None, None, None, None, None), dict, "edit_item" + message)
         self.assertIsInstance(self.shoppingList.delete_item(None, None, None, None), dict, "delete_item" + message)
 
     def test_02_registration_accepts_valid_variables(self):
@@ -49,9 +54,9 @@ class Tests(unittest.TestCase):
         self.assertFalse(result1["success"], "email does not exist in our records")
         self.assertFalse(result2["success"], "wrong email and password combination")
         self.assertTrue(result3["success"], "Login supposed to be successful")
-        self.token = result3["message"]["token"]
-        self.user_id = result3["message"]["user_id"]
-        self.user_name = result3["message"]["user_name"]
+        self.__class__.token = result3["message"]["token"]
+        self.__class__.user_id = result3["message"]["user_id"]
+        self.__class__.user_name = result3["message"]["user_name"]
 
     def test_05_if_provided_token_and_user_id_are_working(self):
         result1 = self.user.logged_in("token", "user_id")
@@ -62,56 +67,52 @@ class Tests(unittest.TestCase):
     def test_06_shopping_list_addition(self):
         result1 = self.shoppingList.add_list("list_name", None, None)
         result2 = self.shoppingList.add_list("list_name", self.user_id, self.token)
-        result3 = self.shoppingList.add_list(None, self.user_id, self.token)
         self.assertFalse(result1["success"], "invalid user_id and token issued")
         self.assertTrue(result2["success"], "Should add list_name, user_id, and token")
-        self.assertFalse(result3["success"], "invalid list_name given")
 
     def test_07_read_shopping_list(self):
         result1 = self.shoppingList.read_lists(self.user_id, self.token)
         self.assertTrue(result1["success"], "success supposed to be true when provided with correct user_id and token")
         self.assertIsInstance(result1["message"], list, "supposed to be a list of lists")
-        self.list = result1["message"][0]
-        self.assertIsInstance(self.list, dict, "expected at least one item return as a dictionary")
+        self.__class__.lst = result1["message"][0]
+        self.assertIsInstance(self.lst, dict, "expected at least one item return as a dictionary")
 
     def test_08_edit_shopping_list(self):
-        result1 = self.shoppingList.update_list(self.list["list_id"], self.token, self.user_id, "new_name")
+        result1 = self.shoppingList.update_list(self.lst["list_id"], self.token, self.user_id, "new_name")
         result2 = self.shoppingList.update_list(None, self.token, self.user_id, "new_name")
         self.assertTrue(result1["success"], "supposed to change the 'list_name' to 'new_name'")
         self.assertFalse(result2["success"], "list_id must exist in the system")
 
     def test_09_add_items_to_the_list(self):
-        result1 = self.shoppingList.add_item("item_name", "2", "", "400", self.list["list_id"], self.token, self.user_id)
+        result1 = self.shoppingList.add_item("item_name", "2", "", "400", self.lst["list_id"], self.token, self.user_id)
         result2 = self.shoppingList.add_item("item_name", "", "kg", "", None, self.token, self.user_id)
-        result3 = self.shoppingList.add_item(None, "", "", "", self.list["list_id"], self.token, self.user_id)
+        result3 = self.shoppingList.add_item(None, "", "", "", self.lst["list_id"], self.token, self.user_id)
         self.assertTrue(result1["success"], "supposed to add item")
         self.assertFalse(result2["success"], "supposed to have a list_id")
         self.assertFalse(result3["success"], "supposed to have a list_name")
 
     def test_10_read_items(self):
-        result1 = self.shoppingList.read_items(self.user_id, self.token, self.list["list_id"])
+        result1 = self.shoppingList.read_items(self.user_id, self.token, self.lst["list_id"])
         self.assertTrue(result1["success"], "supposed to return true when all the right details are provided")
         self.assertIsInstance(result1["message"], list, "supposed to return a list of items")
-        self.items_list = result1["message"][0]
+        self.__class__.items_list = result1["message"][0]
         self.assertIsInstance(self.items_list, dict, "expected at least one item returned as a dictionary")
 
     def test_11_edit_item(self):
         result1 = self.shoppingList.edit_item(
-            self.items_list["item_id"], self.list["list_id"], self.user_id, self.token, "new_item_name", "", "", "")
-        result2 = self.shoppingList.edit_item(
-            None, None, None, None, None, "", "", ""
-        )
+            self.items_list["item_id"], self.lst["list_id"], self.user_id, self.token, "item_name", "new_item_name")
+        result2 = self.shoppingList.edit_item(None, None, None, None, None, "")
         self.assertTrue(result1["success"], "expected to change the 'item_name' to 'new_item_name'")
         self.assertFalse(result2["success"], "expected to return false")
 
     def test_12_delete_item(self):
-        result1 = self.shoppingList.delete_item(self.items_list["item_id"], self.user_id, self.token, self.list["list_id"])
+        result1 = self.shoppingList.delete_item(self.items_list["item_id"], self.user_id, self.token, self.lst["list_id"])
         result2 = self.shoppingList.delete_item(None, None, None, None)
         self.assertTrue(result1["success"], "expected to remove item and return success as true")
         self.assertFalse(result2["success"], "expected to return false since removing the item fail")
 
     def test_13_delete_shopping_list(self):
-        result1 = self.shoppingList.delete_list(self.list["list_id"], self.user_id, self.token)
+        result1 = self.shoppingList.delete_list(self.lst["list_id"], self.user_id, self.token)
         result2 = self.shoppingList.delete_list(None, None, None)
         self.assertTrue(result1["success"], "should return success as true since the list was deleted")
         self.assertFalse(result2["success"], "should return success as false since the list ")
