1d94d23bec1e590ae55b7a7df71789f95f6ce742
Naomi Mariga <kaburamariga@gmail.com>
2017-10-21 17:23:23 +0300

    Deploy to Heroku

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ running application methods
 """
 from user import User
 import json
+import os
 from flask import Flask, request, Response
 from shoppingList import ShoppingList
 
@@ -240,4 +241,5 @@ def delete_items():
 
 
 if __name__ == '__main__':
-    app.run("0.0.0.0", 8080)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
